f329ccb566cb8d831d5dc2ddad5d8b28fffbc5e6
MSp1d3r <etapasolucoes@gmail.com>
2022-09-28 20:46:23 -0300

    Desafio 1

---
+++ blog-codelandia/js/script.js
@@ -0,0 +1,41 @@
+
+
+
+const liked = (item) => {
+  if (item.classList.contains("far")) {
+    item.classList.remove("far");
+    item.classList.add("fas");
+  } else {
+    item.classList.remove("fas");
+    item.classList.add("far");
+  }
+};
+
+const likes = document.querySelectorAll(".like");
+
+for (let like of likes) {
+  like.addEventListener("click", () => liked(like));
+}
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
+
+
+const input = document.getElementById("search-bar");
+const articles = document.querySelectorAll("article");
+const titles = document.querySelectorAll(".title");
+const tags = document.querySelectorAll(".tag");
+
+function search() {
+  input.addEventListener("keyup", () => {
+    const value = input.value.toLowerCase();
+
+    for (let i = 0; i < articles.length; i++) {
+      const title = titles[i].innerText.toLowerCase();
+      let corresponde = title.toLowerCase().indexOf(value) >= 0;
+      articles[i].style.display = corresponde ? "" : "none";
+    }
+  });
+}
+search();
